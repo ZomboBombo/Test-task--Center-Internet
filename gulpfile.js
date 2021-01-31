68ef9bc8098f80aa1ade8746bfe538145198d385
Sergey Novikov <foxinazombobombo@mail.ru>
2021-01-31 18:14:51 +0300

    Adds new tasks for Gulp
    New tasks have been added to gulpfile.js
    
    Added tasks:
    ---
    
    1) "scripts" — to minify JS-code;
    
    2) "html" — to include SVG-sprite and minify HTML-code;
    
    3) "sprite" — to create an SVG-sprite;
    
    4) "clean" — to delete the Build directory;
    
    5) "copy" — to copy all files from the Source directory to the Build directory;
    
    6) "build" — to create the Build directory;

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const gulp = require('gulp');
 
 // --- Вспомогательные утилиты ---
 const rename = require('gulp-rename');
+var del = require("del");
+var svgstore = require("gulp-svgstore");
 
 // --- Препроцессорные утилиты ---
 const sass = require('gulp-sass');
@@ -20,6 +22,11 @@ const csso = require('gulp-csso');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
 const pipeline = require('readable-stream').pipeline;
+
+// --- HTML-утилиты ---
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var htmlmin = require("gulp-htmlmin");
 
 // --- Серверные утилиты ---
 const server = require('browser-sync').create();
@@ -42,12 +49,52 @@ gulp.task('css', () => {
     postcss([
       autoprefixer()
     ]),
-    gulp.dest('source/css'),
+    gulp.dest('build/css'),
     csso(),
     rename('styles.min.css'),
     sourcemap.write('.'),
-    gulp.dest('source/css'),
+    gulp.dest('build/css'),
     server.stream()
+  );
+});
+
+
+// *** Обработка JS-файлов ***
+gulp.task("scripts", function () {
+  return pipeline(
+    gulp.src("source/js/**/*.js"),
+    terser(),
+    concat("scripts.min.js"),
+    gulp.dest("build/js")
+  );
+});
+
+
+// *** Сборка SVG-спрайта ***
+gulp.task('sprite', () => {
+  return pipeline(
+    gulp.src('source/img/icon-*.svg'),
+    svgstore({
+      inlineSvg: true
+    }),
+    rename('sprite.svg'),
+    gulp.dest('build/img')
+  );
+});
+
+
+// *** Обработка HTML ***
+gulp.task('html', () => {
+  return pipeline(
+    gulp.src('source/*.html'),
+    posthtml([
+      include()
+    ]),
+    htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }),
+    gulp.dest('build')
   );
 });
 
@@ -55,7 +102,7 @@ gulp.task('css', () => {
 // *** Работа с Сервером ***
 gulp.task('server', () => {
   server.init({
-    server: 'source/',
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
@@ -63,6 +110,7 @@ gulp.task('server', () => {
   });
 
   gulp.watch('source/sass/**/*.{sass,scss}', gulp.series('css'));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch('source/js/*.js', gulp.series('refresh'));
   gulp.watch('source/*.html', gulp.series('refresh'));
 });
@@ -73,6 +121,35 @@ gulp.task('refresh', (done) => {
 });
 
 
+// *** Очищение директории build/ ***
+gulp.task('clean',  () => {
+  return del('build');
+});
+
+
+// *** Копирование файлов в build/ ***
+gulp.task('copy', () => {
+  return pipeline(
+    gulp.src([
+      'source/fonts/**/*.{woff,woff2}',
+      'source/img/**', '!source/img/icon-*.svg',
+    ], {
+      base: 'source'
+    }),
+    gulp.dest('build')
+  );
+});
+
+
 
 // === Главные задачи для Сборки проекта в "продакшн" и поднятия Сервера ===
-gulp.task('start', gulp.series('server'));
+gulp.task('build', gulp.series(
+  'clean',
+  'copy',
+  'css',
+  'scripts',
+  'sprite',
+  'html'
+));
+
+gulp.task('start', gulp.series('build', 'server'));
